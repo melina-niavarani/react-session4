c82574309e3e9b17e78c3601015f92a96dbac91c
melina-niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-09-12 21:37:39 +0330

    somit array

---
+++ array.ts
@@ -0,0 +1,26 @@
+let weekdays = [
+    "saturday",
+    "sunday",
+    "monday",
+    "thusday",
+    "wednesday",
+    "thursday",
+    "friday"
+]
+console.log(weekdays)
+console.log(weekdays[1])
+
+
+
+let my_name = "maryam";
+function hello() {
+    return "salam"
+}
+const random_things = [100, my_name, hello, null, "mojtaba"]
+console.log(random_things)
+console.log(my_name)
+console.log(random_things[2])
+console.log(random_things[2]())
+random_things.push("babak")
+console.log(random_things)
+
