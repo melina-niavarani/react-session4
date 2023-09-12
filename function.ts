29d653fb4cfee23d8e5297f48fe2aca3dce16f05
melina-niavarani <91658429+melina-niavarani@users.noreply.github.com>
2023-09-12 20:35:41 +0330

    first commit

---
+++ function.ts
@@ -0,0 +1,50 @@
+
+// function hello(name: string, location:string){
+//     console.log("Hi,", name)   
+//     console.log("From:", location)
+//     return "salam";
+// }
+
+// const melina = hello("melina", "iran")
+
+// hello("melina", "iran")
+// console.log(melina)
+
+// function welcome(name: string, location: string){
+//     let welcome_message: string = " ";
+//     welcome_message += "Hello " + name;
+//     welcome_message += " From: " + location
+//     return welcome_message
+// }
+// const greeting = welcome("nariman", "kish")
+// console.log(greeting)
+
+// =============================================
+
+// function circumference(r: number) {
+//     const p = 3.14
+//     return 2 * p * r
+// }
+
+// let a = circumference(10)
+// console.log(a)
+
+
+// ==============================================
+
+// function areaOfCircle(r: number): number{
+//     const p = 3.14
+//     return (r ** 2) * p
+// }
+
+// let area = areaOfCircle(10)
+// console.log(area)
+
+// ==================================mohit nim dayere=====
+function circumference(r: number): number {
+    const p = 3.14
+    return (p * r) + (2 * r)
+}
+
+let nimDayere = circumference(10)
+console.log(nimDayere)
---
+++ session4.ts
@@ -0,0 +1,42 @@
+// let counter: number = 0
+// while ( counter <= 10 ) {
+//     console.log("current value is:", counter)
+//     counter ++
+// }
+
+// for (let counter = 4 ; counter <= 10 ; counter++) {
+//     console.log("current value is:", counter)
+// }
+
+// =============================
+
+// let output: string = "";
+// let output2: string = "";
+// for (let counter = 0; counter < 10; counter++) {
+//     output += "*"
+//     for (let counter = 0; counter < 10; counter++) {
+//         output += " "
+//     }
+//     output += "*\n"
+// }
+// console.log(output)
+// console.log(output2)
+
+// ==================================
+// let bomb:number = 60;
+
+// while (bomb > 0) {
+//     bomb-- ;
+//     console.log("remaining time:", bomb)
+// }
+// console.log("Explosion")
+
+// =================================
+
+// for (let capacity = 10; capacity > 0; capacity--) {
+//     console.log("Welcome")
+// }
+// console.log("Reched full capacity")
+
+// ===================================
+
